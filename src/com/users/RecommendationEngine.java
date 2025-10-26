24791cdb9ee456b200e28e0ccd3931045f846bf6
Phạm Văn Quyết <vanquyetp823@gmail.com>
2025-10-26 17:05:22 +0700

    Cập nhật lớp RecommendationEngine

--- src/com/users/RecommendationEngine.java
+++ src/com/users/RecommendationEngine.java
@@ -1,4 +1,44 @@
 package com.users;
+
+import com.musicPlayer.Song;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class RecommendationEngine {
+
+    // Gợi ý list bài hát user nghe nhiều nhất
+    public List<Song> suggestBasedOnMostPlayed(History history, int numberOfSuggestions) {
+        if (numberOfSuggestions <= 0) {
+            return new ArrayList<>();
+        }
+
+        List<Song> mostPlayed = history.suggestMostPlayed(numberOfSuggestions);
+
+        return mostPlayed;
+    }
+
+    // Gợi ý list bài hát mọi người nghe nhiều nhất
+    public List<Song> suggestBasedOnGlobalTrending(int numberOfSuggestions) {
+        if (numberOfSuggestions <= 0) {
+            return new ArrayList<>();
+        }
+
+        List<Song> trendingMap = History.getGlobalTrending(numberOfSuggestions);    
+        
+        return trendingMap;
+    }
     
+    // Lấy danh sách n bài hát gợi ý (sắp xếp ngẫu nhiên)
+    public List<Song> getListOfSuggestedSongs(History history, int n) {
+        Random rand = new Random();
+        int tmp = rand.nextInt(n) + 1;
+
+        List<Song> suggestions = suggestBasedOnGlobalTrending(tmp);
+        suggestions.addAll(suggestBasedOnMostPlayed(history, n - tmp));
+        Collections.shuffle(suggestions);
+
+        return suggestions;
+    }
 }
